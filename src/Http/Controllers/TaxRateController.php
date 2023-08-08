ec5e3ec451836f9792487b1c37335c1337e42f86
Deniz Tezcan <10155092+deniztezcan@users.noreply.github.com>
2023-08-08 17:01:46 +0200

    Update TaxRateController.php

--- src/Http/Controllers/TaxRateController.php
+++ src/Http/Controllers/TaxRateController.php
@@ -8,6 +8,7 @@ use VarenykyECom\Repositories\TaxRateRepository;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
+use Varenyky\Http\Controllers\BaseController;
 
 class TaxRateController extends BaseController
 {
