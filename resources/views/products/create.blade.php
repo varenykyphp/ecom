b3f1ee65a25eb9b477c9b3cd8051cd9f5c2e196c
Gilbert <138232735+gilbert-nopulp@users.noreply.github.com>
2023-09-08 13:46:08 +0200

    added tiny to description field

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -57,7 +57,7 @@
 
                 <div class="form-group mb-3">
                     <label for="description" class="@if ($errors->has('description')) text-danger @endif">{{ __('VarenykyECom::labels.description') }}</label>
-                    <textarea id="description" placeholder="{{ __('VarenykyECom::labels.description') }}..." name="description" class="form-control @if ($errors->has('description')) is-invalid @endif" rows="4">{{ old('description') }}</textarea>
+                    <textarea id="description" class="tiny form-control" placeholder="{{ __('VarenykyECom::labels.description') }}..." name="description" class="form-control @if ($errors->has('description')) is-invalid @endif" rows="4">{{ old('description') }}</textarea>
                 </div>
 
                 <div class="form-group mb-3">
@@ -115,3 +115,32 @@
     </div>
 </form>
 @endsection
+
+@section('js')
+<script>
+    $(document).ready(function(){
+        @php
+            if (in_array(request()->server('REMOTE_ADDR'), ['127.0.0.1'])) {
+                $plugins = '"link lists link table hr wordcount code"';
+            } else {
+                $plugins = "'link lists link table hr wordcount code'";
+            }
+        @endphp
+
+        tinymce.init({
+            selector: '.tiny',
+            height: 250,
+            plugins: {!! $plugins !!},
+            toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link hr paste wordcount",
+            paste_data_images: true,
+            setup: function(editor) {
+                editor.on("change keyup", function(e) {
+                    tinyMCE.triggerSave();
+                    editor.save();
+                    window.$(editor.getElement()).trigger('change');
+                });
+            }
+        });
+    });
+</script>
+@stop
